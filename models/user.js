fbb29e877ea8671b0cdf3d7a18ae4f9a6da51402
Evelyn <87937888+evelyn-zhan@users.noreply.github.com>
2025-01-16 20:27:18 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,10 +4,12 @@ const User = sequelize.define('user',
     {
         email: DataTypes.STRING,
         password: DataTypes.STRING,
-        active: DataTypes.TINYINT
-    }, {
-        timestamps: false
+        active: DataTypes.TINYINT,
+        username: DataTypes.STRING
     }
+    // , {
+    //     timestamps: false
+    // }
 )
 
 export default User
